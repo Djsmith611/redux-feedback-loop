7ffc20945d1997913f1ec130f57de10643963971
David Smith <djsmith611@me.com>
2024-04-19 10:28:36 -0700

    set up FormFeeling

--- src/components/Form/FormFeeling.jsx
+++ src/components/Form/FormFeeling.jsx
@@ -3,13 +3,29 @@ import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router";
 
 function FormFeeling() {
+    const [feeling, setFeeling] = useState(null);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        
+        if (feeling !== null) {
+            let action = {
+                type:"SET_FEELING",
+                payload:feeling,
+            }
+            dispatch(action);
+            navigate("/form/comments");
+        } else {
+            alert("Please select a feeling value.");
+        }
+    }
+    const handleChange = (e) => {
+        setFeeling(e.target.value);
     }
     return (
         <form onSubmit={e => handleSubmit(e)}>
-            <input type="text" value={feeling}  />
+            <input type="number" value={feeling} onChange={handleChange}  />
 
         </form>
     )
