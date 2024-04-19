7bcc1f3cd557f73b454506040342e442e939ac80
David Smith <djsmith611@me.com>
2024-04-19 10:21:40 -0700

    set up FormComprehension

--- src/components/Form/FormComprehension.jsx
+++ src/components/Form/FormComprehension.jsx
@@ -1,12 +1,33 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router";
+
 function FormComprehension() {
+    const [comprehension, setComprehension] = useState(null);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
     const handleSubmit = (e) => {
         e.preventDefault();
+        if (comprehension !== null) {
+            let action = {
+                type:"SET_COMPREHENSION",
+                payload:comprehension,
+            }
+            dispatch(action);
+            navigate("/form/support");
+        } else {
+            alert("Please select a comprehension value.");
+        }
+    }
 
+    const handleChange = (e) => {
+        setComprehension(e.target.value)
     }
     return (
         <form onSubmit={e => handleSubmit(e)}>
-            <input type="text" value={comprehension} onChange={e => setComprehension(e.target.value)}  />
-            <button>Next</button>
+            <input type="number" value={comprehension} onChange={handleChange}  />
+            <button type="submit">Next</button>
         </form>
     )
 }
--- src/components/Form/FormFeeling.jsx
+++ src/components/Form/FormFeeling.jsx
@@ -1,3 +1,7 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router";
+
 function FormFeeling() {
     const handleSubmit = (e) => {
         e.preventDefault();
